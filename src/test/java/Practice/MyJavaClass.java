94f4bff78056b1949b779348c0458056ba4b064e
Vemula Harika <Vemula Harika@FFE-Vemula-Harika>
2025-08-22 15:11:56 +0530

    COMMITTED

---
+++ src/test/java/Practice/MyJavaClass.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class MyJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HII IAM HARIKKA");
+
+	}
+
+}
