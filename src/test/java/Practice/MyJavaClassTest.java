e5be794a683e96ab670f733572bf058941fb0b71
Vemula Harika <Vemula Harika@FFE-Vemula-Harika>
2025-08-23 11:58:02 +0530

    commit

---
+++ src/test/java/Practice/MyJavaClassTest.java
@@ -0,0 +1,15 @@
+package Practice;
+
+public class MyJavaClassTest {
+
+
+		// TODO Auto-generated method stub
+	public void print() {
+		System.out.println("Hi  java Iam JENKIN");
+	}
+		
+		
+
+	
+
+}
